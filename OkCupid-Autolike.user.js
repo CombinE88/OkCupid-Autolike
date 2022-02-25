926c1cb8ae0e0ca65dcd12630f107b1b34e8d432
Roland Zipper <Roland.Zipper@web.de>
2022-02-25 20:36:20 +0100

    Update OkCupid-Autolike.user.js

--- OkCupid-Autolike.user.js
+++ OkCupid-Autolike.user.js
@@ -2,15 +2,15 @@
 // Radom Liker for Okcupied
 // Made by CombinE88 & Pr0xy-F0x
 
-setInterval(function(){
+while(true){
     var desktopButton = document.getElementsByClassName("dt-action-buttons-button like")[0];
     (desktopButton).click();
     await new Promise(r => setTimeout(r, getRandomNumber()));
 
-},1)
+}
 
 function getRandomNumber(){
-    var multiplicator = Math.random()*1000;
+    var multiplicator = Math.random()*3000;
     var number = Math.random();
     var outPut = Math.ceil(number*multiplicator);
     return outPut;
